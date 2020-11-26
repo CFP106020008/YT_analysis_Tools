8743d53296572be5c366463fad131b9d228ba77c
Lin Yen-Hsing <julius52700@gapp.nthu.edu.tw>
2020-11-26 20:34:17 +0800

    fixed problem with movies

---
+++ CM2.py
@@ -0,0 +1,103 @@
+import numpy as np
+import yt
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from matplotlib import rc_context
+from mpl_toolkits.axes_grid1 import AxesGrid
+
+#Set the parameters
+Folder_p = "./AGNCRp/crbub_hdf5_plt_cnt_*"
+Folder_e = "./AGNCRe/crbub_hdf5_plt_cnt_*"
+#Field = 'density'
+#Field = 'CR_energy_density'
+#Field = 'pressure'
+Field = 'temperature'
+CMAP = 'algae' #'dusk'
+FPS = 20
+#===========================#
+fig = plt.figure(figsize=(16,9))
+
+ts_p = yt.load(Folder_p) #Proton Jet dataset
+ts_e = yt.load(Folder_e) #Electron Jet dataset
+
+start_frame = 0
+end_frame = min(len(ts_p),len(ts_e))
+
+fns = [ts_p, ts_e] # Total set of datas
+def ecr(field, data):
+    return data["density"]*data["cray"]*yt.YTQuantity(1,"erg/g")
+
+yt.add_field(function = ecr, 
+             units = "erg/cm**3", 
+             name = "CR_energy_density", 
+             sampling_type = "cell")
+
+#fig = plt.figure(figsize=(16,9))
+rc_context({'mathtext.fontset': 'stix'})
+grid = AxesGrid(fig, (0.090,0.075,0.80,0.85),
+                nrows_ncols = (1, 2),
+                axes_pad = 0.5,
+                label_mode = "L",
+                share_all = True,
+                cbar_location = "right",
+                cbar_mode = "single",
+                cbar_size = "3%",
+                cbar_pad = "0%")
+
+# Proton Jet        
+pp = yt.SlicePlot(ts_p[start_frame], 
+                 'x', 
+                 Field, 
+                 width=(200, 'kpc')
+                 ).set_cmap(field = Field, cmap=CMAP
+                 )#.annotate_velocity(factor = 16,normalize=True)
+
+#pp.set_zlim(Field, 1e-40, 1e0) # For ecr
+#pp.set_zlim(Field, 1e-27, 1e-24) # For den
+#pp.set_zlim(Field, 1e-11, 1e-8) # For pressure
+pp.set_zlim(Field, 1e7, 1e9) # For temp
+plotp = pp.plots[Field]        
+plotp.figure = fig
+plotp.axes = grid[0].axes
+plotp.cax = grid.cbar_axes[0]
+pp._setup_plots()
+
+# Electron Jet
+
+pe = yt.SlicePlot(ts_e[start_frame], 
+                 'x', 
+                 Field, 
+                 width=(200, 'kpc')
+                 ).set_cmap(field = Field, cmap=CMAP
+                 )#.annotate_velocity(factor = 16,normalize=True)
+
+#pe.set_zlim(Field, 1e-40, 1e0) # For ecr
+#pe.set_zlim(Field, 1e-27, 1e-24) # For den
+#pe.set_zlim(Field, 1e-11, 1e-8) # For pressure
+pe.set_zlim(Field, 1e7, 1e9) # For temp
+plote = pe.plots[Field]        
+plote.figure = fig
+plote.axes = grid[1].axes
+plote.cax = grid.cbar_axes[1]
+pp._setup_plots()
+
+plt.title("Proton Jet / Electron Jet")
+
+#plt.savefig("./temp-store/{}_frame={:04d}.png".format(Field,j),dpi=300)
+
+def animate(i):
+    print("Making Video: {}/{}".format(i+1,len(ts_p)))
+    pp._switch_ds(ts_p[i])
+    pe._switch_ds(ts_e[i])
+    grid[0].axes.set_title("Proton Jet", fontsize=20)
+    grid[1].axes.set_title("Electron Jet", fontsize=20)
+    plt.suptitle("Time: {} Myr".format(ts_p[i].current_time/31556926e6))
+
+animation = FuncAnimation(fig = fig, 
+                          func = animate, 
+                          frames = range(start_frame,end_frame),
+                          interval = int(1000/FPS), 
+                          save_count = 0
+                          )
+
+animation.save('Comparison_{}.mp4'.format(Field), dpi=300)
--- Comparison_Movie.py
+++ Comparison_Movie.py
@@ -8,29 +8,18 @@ from mpl_toolkits.axes_grid1 import AxesGrid
 #Set the parameters
 Folder_p = "./AGNCRp/crbub_hdf5_plt_cnt_*"
 Folder_e = "./AGNCRe/crbub_hdf5_plt_cnt_*"
-Fields = ['CR_energy_density']#, 'density', 'pressure', 'temperature']
+Fields = ['density']#, 'CR_energy_density', 'pressure', 'temperature']
 CMAP = 'algae' #'dusk'
 FPS = 20
-SAVE_IMAGE = True
+SAVE_IMAGE = False
 
 #===========================#
-
-ts_p = yt.load(Folder_p)
-ts_e = yt.load(Folder_e)
-fns = [ts_p, ts_e]
-
 fig = plt.figure(figsize=(16,9))
-rc_context({'mathtext.fontset': 'stix'})
-grid = AxesGrid(fig, (0.090,0.075,0.80,0.85),
-                nrows_ncols = (1, 2),
-                axes_pad = 0.5,
-                label_mode = "L",
-                share_all = True,
-                cbar_location = "right",
-                cbar_mode = "single",
-                cbar_size = "3%",
-                cbar_pad = "0%")
 
+ts_p = yt.load(Folder_p) #Proton Jet dataset
+ts_e = yt.load(Folder_e) #Electron Jet dataset
+
+fns = [ts_p, ts_e] # Total set of datas
 def ecr(field, data):
     return data["density"]*data["cray"]*yt.YTQuantity(1,"erg/g")
 
@@ -41,6 +30,18 @@ yt.add_field(function = ecr,
 
 def One_frame(j):
     
+    #fig = plt.figure(figsize=(16,9))
+    rc_context({'mathtext.fontset': 'stix'})
+    grid = AxesGrid(fig, (0.090,0.075,0.80,0.85),
+                    nrows_ncols = (1, 2),
+                    axes_pad = 0.5,
+                    label_mode = "L",
+                    share_all = True,
+                    cbar_location = "right",
+                    cbar_mode = "single",
+                    cbar_size = "3%",
+                    cbar_pad = "0%")
+    
     for i, fn in enumerate(fns):
     
         # Load the data and create a single plot        
@@ -48,19 +49,14 @@ def One_frame(j):
                          'x', 
                          Field, 
                          width=(200, 'kpc')
-                        ).set_cmap(field = Field, cmap=CMAP
-                        )#.annotate_velocity(factor = 16,normalize=True)
+                         ).set_cmap(field = Field, cmap=CMAP
+                         )#.annotate_velocity(factor = 16,normalize=True)
 
         # Ensure the colorbar limits match for all plots
-        #p.set_zlim(Field, 1e-40, 1e0)
+        #p.set_zlim(Field, 1e-40, 1e0) # For ecr
+        p.set_zlim(Field, 1e-27, 1e-24) # For den
 
-        # This forces the ProjectionPlot to redraw itself on the AxesGrid axes.
-        
-        #frb = p.to_frb((200, "kpc"), 800)
-        #grid[i].axes.imshow(np.array(frb[Field]))
-        
-        plot = p.plots[Field]
-        
+        plot = p.plots[Field]        
         plot.figure = fig
         plot.axes = grid[i].axes
         plot.cax = grid.cbar_axes[i]
@@ -73,20 +69,29 @@ def One_frame(j):
         grid[i].axes.set_title(Name, fontsize=20)
     
     if SAVE_IMAGE:
-        plt.savefig("./temp-store/{}_frame={}.png".format(Field,j),dpi=300)
+        plt.savefig("./temp-store/{}_frame={:04d}.png".format(Field,j),dpi=300)
+
     return fig
 
 def animate(i):
     print("Making Video: {}/{}".format(i+1,len(ts_p)))
+    p.
+    
+    
     One_frame(i)
     #plt.close()
 
 animation = FuncAnimation(fig = fig, 
                           func = animate, 
-                          frames = min(len(ts_p),len(ts_e)),
-                          interval = int(1000/FPS)
+                          frames = 3,#min(len(ts_p),len(ts_e)),
+                          interval = int(1000/FPS), 
+                          save_count = 0
                           )
 
 for Field in Fields:
     #One_frame(30)
     animation.save('Comparison_{}.mp4'.format(Field), dpi=300)
+    #for i in range(min(len(ts_p),len(ts_e))):
+    #    One_frame(i)
+    #    plt.close()
+    #    print("Making Video: {}/{}".format(i+1,len(ts_p)))
