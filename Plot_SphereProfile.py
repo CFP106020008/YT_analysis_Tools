75fa1aa309d72d7d4e0559edc320ba66884112a1
CFP106020008 <julius52700@gapp.nthu.edu.tw>
2020-12-27 16:04:52 +0800

    New Plot_Spherical.py

--- Plot_SphereProfile.py
+++ Plot_SphereProfile.py
@@ -9,58 +9,68 @@ import My_Plugin as M
 from matplotlib.animation import FuncAnimation
 
 #Set the parameters
-Folder_p = "./AGNCRp/crbub_hdf5_plt_cnt_*"
-Folder_e = "./AGNCRe/crbub_hdf5_plt_cnt_*"
+Folder_p = "./CRp/crbub_hdf5_plt_cnt_*"
+#Folder_e = "./AGNCRe/crbub_hdf5_plt_cnt_*"
 #Field = ['Heating/Cooling']
 #Field = 'CR_energy_density'
 #Field = 'density'
 #Field = 'pressure'
 #Field = 'temperature'
-CMAP = 'seismic'#'algae' #'dusk'
+#CMAP = 'seismic'#'algae' #'dusk'
 Frames = [1,10,20,30,40,50]
 radius = 50
 center = [0,0,0]
 FPS = 10
-ANIME = False
+ANIME = True
 #===========================#
 
 ts_p = yt.load(Folder_p) #Proton Jet dataset
-ts_e = yt.load(Folder_e) #Electron Jet dataset
+#ts_e = yt.load(Folder_e) #Electron Jet dataset
 start_frame = 1
-end_frame = min(len(ts_p),len(ts_e))
+end_frame = len(ts_p)
 
-fns = [ts_p, ts_e] # Total set of datas
+#fns = [ts_p, ts_e] # Total set of datas
 fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(8,4.5), sharey=True)
+c, = ax.semilogy([], [], label="Cooling Rate", color='b')
+h, = ax.semilogy([], [], label="Heating Rate", color='r')
+ax.set_xlabel("Radius (kpc)")
+ax.set_ylabel(r'$(erg/s/cm^3)$')
+ax.set_xlim([0,radius])
+ax.set_ylim([1e-28,1e-20])
  
-def Plot(frame, fig, ax):    
+def Plot(frame):    
     ds = ts_p[frame]
     sphere = ds.sphere(center, (radius, "kpc"))
     P = yt.create_profile( sphere, 'radius', ['cooling_rate','crht'],
                            units = {'radius': 'kpc'},
                            logs = {'radius': False})
     
-    if ANIEM:
-
-    else:
-        fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(8,4.5), sharey=True)
-        #plt.subplots_adjust(left=0.05, bottom=0.2, right=0.89, top=0.9, wspace=0.1, hspace=0.4) 
-        c = ax.semilogy(P.x.value, P['cooling_rate'].value, label="Cooling Rate", color='b')
-        h = ax.semilogy(P.x.value, P['crht'].value, label="Heating Rate", color='r')
-        ax.set_xlabel("Radius (kpc)")
+    if ANIME:
+        #print("ANIEM!!")
+        c.set_data(P.x.value, P['cooling_rate'].value)
+        h.set_data(P.x.value, P['crht'].value)
         ax.set_title("Time: {:03.0f} Myr".format(float(ts_p[frame].current_time/31556926e6)))
-        ax.set_ylabel(r'$(erg/s/cm^3)$')
-        ax.set_ylim([1e-28,1e-20])
-        plt.legend()
+    else:
+        #fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(8,4.5), sharey=True)
+        #plt.subplots_adjust(left=0.05, bottom=0.2, right=0.89, top=0.9, wspace=0.1, hspace=0.4) 
+        #c = ax.semilogy(P.x.value, P['cooling_rate'].value, label="Cooling Rate", color='b')
+        #h = ax.semilogy(P.x.value, P['crht'].value, label="Heating Rate", color='r')
+        c.set_data(P.x.value, P['cooling_rate'].value)
+        h.set_data(P.x.value, P['crht'].value)
+        ax.set_title("Time: {:03.0f} Myr".format(float(ts_p[frame].current_time/31556926e6)))
+        #plt.legend()
         #handles, labels = ax.get_legend_handles_labels()
         #fig.legend(handles, labels)#, loc='center right')
         #plt.suptitle("Time: {:03.0f} Myr".format(float(ts_p[Frame].current_time/31556926e6)))
         plt.savefig("HeatCool_Spherical_Profile_frame={}.png".format(frame), dpi=300)
-        plt.clf()
+        #plt.cla()
+    return c, h
 
-if ANIEM:
+# Main Code
+if ANIME:
     def animate(i):
         print("Making Video: {}/{}".format(i+1,len(ts_p)))
-        Plot(i, fig, ax)
+        Plot(i)
 
     animation = FuncAnimation(fig = fig,
                               func = animate,
@@ -71,6 +81,4 @@ if ANIEM:
     animation.save('HeatCoolProfile.mp4', dpi=300)
 else:
     for frame in Frames:
-        Plot(frame, fig, ax)
-
-
+        Plot(frame)
