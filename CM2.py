1ebd098a62a9a4b6e1a9d467076a9094723d72c4
CFP106020008 <julius52700@gapp.nthu.edu.tw>
2021-01-12 14:18:35 +0800

    functionlize CM2

--- CM2.py
+++ CM2.py
@@ -11,9 +11,10 @@ Folder_p = "./CRp_Streaming/crbub_hdf5_plt_cnt_*"
 Folder_e = "./CRe_Streaming/crbub_hdf5_plt_cnt_*"
 Fields = {  'crht': 0,
             'CR_energy_density': 0,
-            'density': 1,
-            'pressure': 1,
-            'temperature': 1}
+            'density': 0,
+            'pressure': 0,
+            'temperature': 0,
+            'csht': 1}
 CMAP = 'algae' #'dusk'
 FPS = 10
 #===========================#
@@ -22,11 +23,26 @@ ts_p = yt.load(Folder_p) #Proton Jet dataset
 ts_e = yt.load(Folder_e) #Electron Jet dataset
 
 start_frame = 1
-end_frame = min(len(ts_p),len(ts_e))
+end_frame = 10 #min(len(ts_p),len(ts_e))
 
 fns = [ts_p, ts_e] # Total set of datas
 
 rc_context({'mathtext.fontset': 'stix'})
+
+def One_Plot(i, ts, frame):
+    p = yt.SlicePlot(ts[frame], 
+                     'x', 
+                     Field, 
+                     width=(200, 'kpc')
+                     ).set_cmap(field = Field, cmap=CMAP
+                     )#.annotate_velocity(factor = 16,normalize=True)
+
+    p.set_zlim(Field, M.Zlim(Field)[0], M.Zlim(Field)[1])
+    plot = p.plots[Field]        
+    plot.figure = fig
+    plot.axes = grid[i].axes
+    plot.cax = grid.cbar_axes[i]
+    p._setup_plots()
 
 for key in Fields:
     if Fields[key] == 1:
@@ -43,41 +59,17 @@ for key in Fields:
                         cbar_pad = "0%")
 
         # Proton Jet        
-        pp = yt.SlicePlot(ts_p[start_frame], 
-                         'x', 
-                         Field, 
-                         width=(200, 'kpc')
-                         ).set_cmap(field = Field, cmap=CMAP
-                         )#.annotate_velocity(factor = 16,normalize=True)
-
-        pp.set_zlim(Field, M.Zlim(Field)[0], M.Zlim(Field)[1])
-        plotp = pp.plots[Field]        
-        plotp.figure = fig
-        plotp.axes = grid[0].axes
-        plotp.cax = grid.cbar_axes[0]
-        pp._setup_plots()
-
+        One_Plot(0, ts_p, start_frame)
         # Electron Jet
+        One_Plot(1, ts_e, start_frame)
 
-        pe = yt.SlicePlot(ts_e[start_frame], 
-                         'x', 
-                         Field, 
-                         width=(200, 'kpc')
-                         ).set_cmap(field = Field, cmap=CMAP
-                         )#.annotate_magnetic_field()
-                         #.annotate_velocity(factor = 16,normalize=True)
-
-        pe.set_zlim(Field, M.Zlim(Field)[0], M.Zlim(Field)[1])
-        plote = pe.plots[Field]        
-        plote.figure = fig
-        plote.axes = grid[1].axes
-        plote.cax = grid.cbar_axes[1]
-        pe._setup_plots()
 
         def animate(i):
             print("Making Video: {}/{}".format(i+1,len(ts_p)))
-            pp._switch_ds(ts_p[i])
-            pe._switch_ds(ts_e[i])
+            One_Plot(0, ts_p, i)
+            One_Plot(1, ts_e, i)
+            #pp._switch_ds(ts_p[i])
+            #pe._switch_ds(ts_e[i])
             grid[0].axes.set_title("Proton / With Streaming", fontsize=20)
             grid[1].axes.set_title("Electron / With Streaming", fontsize=20)
             fig.suptitle("Time: {:03.0f} Myr".format(float(ts_p[i].current_time/31556926e6)),fontsize=24)
