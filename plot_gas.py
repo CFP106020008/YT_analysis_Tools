0cdfbb84ec25adf5a5e87f9d8a4d78d7d2938bf2
CFP106020008 <julius52700@gapp.nthu.edu.tw>
2022-01-25 01:18:56 +0800

    plot_gas.py can parallelize correctly

--- plot_gas.py
+++ plot_gas.py
@@ -1,4 +1,5 @@
 import yt 
+import glob
 import os
 import matplotlib.pyplot as plt
 from matplotlib import rc_context
@@ -13,9 +14,9 @@ Field = ('gas','density')
 #Field = ('gas','velocity_magnitude')
 
 #Cmap = 'gist_heat'
-Cmap = 'cividis'
+Cmap = 'viridis'
 
-Zlim = [5e-4, 3e-3] # For density
+Zlim = [3e-5, 1e-1] # For density
 #Zlim = [1e33, 5e33] # For internal energy?
 
 # Get the files
@@ -29,8 +30,6 @@ def a_to_t(a):
     z = 1/a - 1
     return cosmo.age(z).value
 
-# Enable parallelization
-yt.enable_parallelism()
 
 # Load data
 unit_base = {'UnitLength_in_cm'         : 3.08568e+24,
@@ -43,13 +42,17 @@ bbox_lim = 50 #Mpc
 bbox = [[0,bbox_lim],
         [0,bbox_lim],
         [0,bbox_lim]]
+
 DS = yt.load(source_path+"*.hdf5", unit_base=unit_base, bounding_box=bbox)
+fns = glob.glob(source_path+"*.hdf5")
+fns.sort()
 
 def Gas_Plot(ds, Field, Cmap='cividis'):
+    print(ds.parameter_filename)
     p = yt.ProjectionPlot(ds, 'x', Field)
     #p = yt.SlicePlot(ds, 'x', Field)
     p.set_cmap(Field, Cmap)
-    #p.set_zlim(Field, Zlim[0], Zlim[1])
+    p.set_zlim(Field, Zlim[0], Zlim[1])
     fig = p.export_to_mpl_figure((1,1))
     ax = fig.axes[0]
     ax.text(0.05, 0.95, 
@@ -60,7 +63,7 @@ def Gas_Plot(ds, Field, Cmap='cividis'):
             verticalalignment='top', 
             transform=ax.transAxes)
     plt.tight_layout()
-    fig.savefig(image_path+"{}.jpg".format(ds.parameter_filename.split(".")[0].split('/')[-1]))
+    fig.savefig(image_path+"{}.jpg".format(ds.parameter_filename.split(".")[0].split('/')[-1]), dpi=300)
 
 # This is main
 def main():
@@ -68,11 +71,20 @@ def main():
         print("No source files! Wrong path!")
     if not(os.path.isdir(image_path)):
         os.system('mkdir ' + image_path)
+    for ds in DS.piter():
+        with rc_context({"mathtext.fontset":"stix"}):
+            Gas_Plot(ds, Field, Cmap=Cmap)
 
-    for i, ds in enumerate(DS.piter()):
+'''
+    for fn in yt.parallel_objects(fns, njobs=5):
+        ds = yt.load(fn, unit_base=unit_base, bounding_box=bbox)
         with rc_context({"mathtext.fontset":"stix"}):
             Gas_Plot(ds, Field, Cmap=Cmap)
             #DM.Plot(source_path+fnames[i])
+'''
+
 if __name__ == '__main__':
+    # Enable parallelization
+    yt.enable_parallelism()
     main()
 
